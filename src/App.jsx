fb1aa86af48c079a44d8e3274c871e08778e545d
kessleru <otaviokesslerustra@hotmail.com>
2026-01-03 00:16:12 -0300

    feat: add SVG assets for icons and update components
    - Added new SVG assets: sair.svg, usuario.svg, visualizacao-black.svg, and visualizacao.svg.
    - Created Footer component with basic structure.
    - Implemented Header component with navigation links and logo.
    - Developed Home component with placeholder text.
    - Created Login component with basic structure.
    - Updated index.css to include Tailwind CSS utilities.
    - Set up main.jsx to render the App component.
    - Configured Vite with React, Tailwind CSS, and SVGR plugins.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
+const App = () => {
+  return (
+    <div className='pt-16'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='login' element={<Login/>}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Dogs from '../Assets/dogs.svg?react';
+
+const Header = () => {
+  return (
+    <header  className='shadow-sm fixed w-full z-50 bg-white top-0'>
+      <nav className='container flex justify-between items-center h-16'>
+        <Link className='inline-block py-2' to='/' aria-label='Dogs - Home'>
+          <Dogs/>
+        </Link>
+        <Link className='text-neutral-800 flex items-center after:content-[""] after:inline-block after:ml-2 after:w-[14px] after:h-[17px] after:bg-[url("../Assets/usuario.svg")] after:bg-no-repeat after:bg-center after:relative after:-top-px' to='login'>Login / Criar</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
