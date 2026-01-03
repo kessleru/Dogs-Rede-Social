1f56ca060fac3447c9104ef34b7001dd7b483266
kessleru <otaviokesslerustra@hotmail.com>
2026-01-03 14:57:18 -0300

    feat: update routing for login and add login components

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordReset from './LoginPasswordReset';
+import LoginPasswordLost from './LoginPasswordLost';
 
 const Login = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route index element={<LoginForm />} />
+        <Route path='criar' element={<LoginCreate />} />
+        <Route path='perdeu' element={<LoginPasswordLost />} />
+        <Route path='resetar' element={<LoginPasswordReset />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
