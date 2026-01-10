0f294f88a6c07f3934252a2d7df69c8dd5e09e67
kessleru <otaviokesslerustra@hotmail.com>
2026-01-10 15:30:58 -0300

    feat: refactor Feed component imports, enhance PhotoComments with comments display and form submission

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import FeedPhotos from './FeedPhotos'
 import FeedModal from './FeedModal'
 
--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useContext, useState } from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => {
+    return props.comments || [];
+  });
+  const { login } = useContext(UserContext);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <ul className=''>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
+};
 
-export default PhotoComments
+export default PhotoComments;
